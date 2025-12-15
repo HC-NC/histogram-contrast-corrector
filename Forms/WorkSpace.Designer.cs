522f47d741055c07a781db37765cc33a2209e1ff
Alexander Dubrovsky <sanya.dubrovskiy@inbox.ru>
2025-12-16 06:49:24 +0700

    Added: opening a image, rendering the first band and calculating its histogram

--- Forms/WorkSpace.Designer.cs
+++ Forms/WorkSpace.Designer.cs
@@ -28,19 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            splitContainer1 = new SplitContainer();
+            plotView1 = new OxyPlot.WindowsForms.PlotView();
+            pictureBox1 = new PictureBox();
+            openFileDialog1 = new OpenFileDialog();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            statusStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(778, 33);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, toolStripSeparator1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(54, 29);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.O;
+            openToolStripMenuItem.Size = new Size(223, 34);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(220, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
+            exitToolStripMenuItem.Size = new Size(223, 34);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.BackColor = SystemColors.ControlDark;
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 33);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(plotView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(pictureBox1);
+            splitContainer1.Size = new Size(778, 479);
+            splitContainer1.SplitterDistance = 366;
+            splitContainer1.TabIndex = 2;
+            // 
+            // plotView1
+            // 
+            plotView1.BackColor = SystemColors.Control;
+            plotView1.Dock = DockStyle.Fill;
+            plotView1.Location = new Point(0, 0);
+            plotView1.Name = "plotView1";
+            plotView1.PanCursor = Cursors.Hand;
+            plotView1.Size = new Size(366, 479);
+            plotView1.TabIndex = 0;
+            plotView1.Text = "plotView1";
+            plotView1.ZoomHorizontalCursor = Cursors.SizeWE;
+            plotView1.ZoomRectangleCursor = Cursors.SizeNWSE;
+            plotView1.ZoomVerticalCursor = Cursors.SizeNS;
+            plotView1.DoubleClick += plotView1_DoubleClick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Control;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(408, 479);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(24, 24);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripStatusLabel2 });
+            statusStrip1.Location = new Point(0, 512);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(778, 32);
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(179, 25);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(179, 25);
+            toolStripStatusLabel2.Text = "toolStripStatusLabel2";
             // 
             // WorkSpace
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(778, 544);
+            Controls.Add(splitContainer1);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "WorkSpace";
-            Text = "Form1";
-            Load += this.WorkSpace_Load;
+            Text = "HCC";
+            Load += WorkSpace_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private SplitContainer splitContainer1;
+        private PictureBox pictureBox1;
+        private OpenFileDialog openFileDialog1;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripStatusLabel toolStripStatusLabel2;
     }
 }
--- Forms/WorkSpace.cs
+++ Forms/WorkSpace.cs
@@ -1,24 +1,120 @@
 using OSGeo.GDAL;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace Histogram_Contrast_Corrector
 {
     public partial class WorkSpace : Form
     {
-        private string _imgPath;
+        private string? _curFilePath;
+
+        private Dataset? _curDS;
 
         public WorkSpace()
         {
             InitializeComponent();
 
-            //_imgPath = @"D:\Институт\ПГИС\Лабораторные ПГИС\1\LC08_L1TP_143021_20211224_20211230_01_T1\LC08_L1TP_143021_20211224_20211230_01_T1_B2.TIF";
-            _imgPath = @"D:\Институт\ПГИС\Лабораторные ПГИС\1\LC08_L1TP_143021_20211224_20211230_01_T1\LC08.tif";
+            toolStripStatusLabel1.Text = "";
+            toolStripStatusLabel2.Text = "";
+
+            openFileDialog1.Filter = "All files|*.*|TIFF|*.tif";
+        }
+
+        ~WorkSpace()
+        {
+            _curDS?.Dispose();
         }
 
         private void WorkSpace_Load(object sender, EventArgs e)
         {
-            Dataset ds = Gdal.Open(_imgPath, Access.GA_ReadOnly);
+        }
 
-            MessageBox.Show(ds.RasterCount.ToString());
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _curFilePath = openFileDialog1.FileName;
+                toolStripStatusLabel1.Text = "Current: " + openFileDialog1.SafeFileName;
+
+                _curDS = Gdal.Open(_curFilePath, Access.GA_ReadOnly);
+                Draw(_curDS.GetRasterBand(1));
+            }
+        }
+
+        private void Draw(Band band)
+        {
+            double[] argout = new double[2];
+
+            band.ComputeRasterMinMax(argout, 0);
+
+            band.GetMinimum(out double maxV, out _);
+            band.GetMaximum(out double minV, out _);
+
+            double[] values = new double[band.XSize * band.YSize];
+
+            band.ReadRaster(0, 0, band.XSize, band.YSize, values, band.XSize, band.YSize, 0, 0);
+
+            foreach (double v in values)
+            {
+                if (v == 0)
+                    continue;
+
+                minV = Math.Min(minV, v);
+                maxV = Math.Max(maxV, v);
+            }
+
+            toolStripStatusLabel2.Text = $"XSize: {band.XSize} YSize: {band.YSize} Min: {minV} Max: {maxV}";
+
+            int[] histogram = new int[(int)(maxV - minV) + 1];
+            Bitmap bitmap = new Bitmap(band.XSize, band.YSize);
+
+            for (int y = 0; y < band.YSize; y++)
+            {
+                for (int x = 0; x < band.XSize; x++)
+                {
+                    double v = values[y * band.XSize + x];
+
+                    if (v == 0)
+                    {
+                        bitmap.SetPixel(x, y, Color.Black);
+                        continue;
+                    }
+
+                    histogram[(int)(v - minV)] += 1;
+
+                    int c = (int)((v - minV) / (maxV - minV) * 255);
+                    Color color = Color.FromArgb(c, c, c);
+                    bitmap.SetPixel(x, y, color);
+                }
+            }
+
+            var series = new HistogramSeries();
+
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                series.Items.Add(new HistogramItem(i + minV, i + minV + 1, histogram[i], 0));
+            }
+
+            PlotModel plot = new PlotModel();
+            plot.Series.Add(series);
+
+            plot.Axes.Add(new LinearAxis() { Position = AxisPosition.Bottom, Minimum = minV, Maximum = maxV });
+            plot.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, Minimum = 0, Maximum = histogram.Max() });
+
+            plotView1.Model = plot;
+            pictureBox1.Image = bitmap;
+        }
+
+        private void plotView1_DoubleClick(object sender, EventArgs e)
+        {
+            plotView1.Model.ResetAllAxes();
+            plotView1.Refresh();
         }
     }
 }
